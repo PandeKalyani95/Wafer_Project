915d8febca6107019866a0d63963aa66bf478526
PandeKalyani95 <kalyanipande95@gmail.com>
2021-04-13 17:01:46 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It's a wafer Ml project using mlops',
+    author='Kalyani0395',
+    license='MIT',
+)
